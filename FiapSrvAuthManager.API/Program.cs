a1f6364fb88e0ab8a56cc330b6cc54302593e828
JPedroDuarte23 <jpedroduarte12345@gmail.com>
2025-09-22 21:21:40 -0300

    Alterando provedor de cloud

--- FiapSrvAuthManager.API/Program.cs
+++ FiapSrvAuthManager.API/Program.cs
@@ -1,7 +1,8 @@
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-using FiapSrvAuthManager.Application.Services;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using AspNetCore.DataProtection.Aws.S3;
 using FiapSrvAuthManager.Application.Interface;
+using FiapSrvAuthManager.Application.Services;
 using FiapSrvAuthManager.Infrastructure.Configuration;
 using FiapSrvAuthManager.Infrastructure.Mappings;
 using FiapSrvAuthManager.Infrastructure.Middleware;
@@ -19,79 +20,82 @@ var builder = WebApplication.CreateBuilder(args);
 Log.Logger = SerilogConfiguration.ConfigureSerilog();
 builder.Host.UseSerilog();
 
+// 1. Configuração da AWS
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonSimpleSystemsManagement>();
+
 string mongoConnectionString;
-string databaseName = builder.Configuration.GetSection("MongoDbSettings:DatabaseName").Value ?? "";
 string jwtSigningKey;
+string databaseName = builder.Configuration["MongoDbSettings:DatabaseName"]!;
 
 if (!builder.Environment.IsDevelopment())
 {
+    Log.Information("Ambiente de Produção. Buscando segredos do AWS Parameter Store.");
+    var ssmClient = new AmazonSimpleSystemsManagementClient();
 
-    var keyVaultUrl = builder.Configuration["KeyVault:Url"];
-    var secretName = builder.Configuration["KeyVault:DatabaseSecretName"];
-    var blobUrl = builder.Configuration["Blob:Url"];
-    var keyName = builder.Configuration["KeyVault:BlobKeyName"];
-    var jwtSecretName = builder.Configuration["KeyVault:JwtSigningKeyName"];
+    // Busca a Connection String do MongoDB
+    var mongoParameterName = builder.Configuration["ParameterStore:MongoConnectionString"];
+    var mongoResponse = await ssmClient.GetParameterAsync(new GetParameterRequest
+    {
+        Name = mongoParameterName,
+        WithDecryption = true
+    });
+    mongoConnectionString = mongoResponse.Parameter.Value;
 
-    var credential = new DefaultAzureCredential();
-    var managedCredential = new ManagedIdentityCredential();
+    // Busca a Chave de Assinatura do JWT
+    var jwtParameterName = builder.Configuration["ParameterStore:JwtRsaPrivateKey"];
+    var jwtResponse = await ssmClient.GetParameterAsync(new GetParameterRequest
+    {
+        Name = jwtParameterName,
+        WithDecryption = true
+    });
+    jwtSigningKey = jwtResponse.Parameter.Value;
 
-    var client = new SecretClient(
-        new Uri(keyVaultUrl),
-        credential
-    );
+    // 2. Configuração do Data Protection com AWS S3
+    var s3Bucket = builder.Configuration["DataProtection:S3BucketName"];
+    var s3KeyPrefix = builder.Configuration["DataProtection:S3KeyPrefix"];
+    var s3DataProtectionConfig = new S3XmlRepositoryConfig(s3Bucket) { KeyPrefix = s3KeyPrefix };
 
     builder.Services.AddDataProtection()
-        .SetApplicationName("FiapSrvGames")
-        .PersistKeysToAzureBlobStorage(new Uri(blobUrl), managedCredential)
-        .ProtectKeysWithAzureKeyVault(new Uri(keyVaultUrl + "keys/" + keyName), managedCredential);
-
-
-    KeyVaultSecret jwtKey = await client.GetSecretAsync(jwtSecretName);
-    jwtSigningKey = jwtKey.Value;
-
-    KeyVaultSecret mongoConnectionSecret = await client.GetSecretAsync(secretName);
-    mongoConnectionString = mongoConnectionSecret.Value;
-
+        .SetApplicationName("FiapSrvAuthManager")
+        .PersistKeysToAwsS3(s3DataProtectionConfig);
 }
 else
 {
-    Log.Information("Ambiente de Desenvolvimento/Local detectado. Obtendo string de conexao do appsettings. ??");
-    mongoConnectionString = builder.Configuration.GetSection("MongoDbSettings:ConnectionString").Value ?? "";
-    jwtSigningKey = builder.Configuration.GetSection("Jwt:DevKey").Value ?? "";
+    Log.Information("Ambiente de Desenvolvimento. Usando appsettings.json.");
+    // Adicione a connection string de desenvolvimento ao seu appsettings.Development.json
+    mongoConnectionString = builder.Configuration.GetConnectionString("MongoDbConnection")!;
+    jwtSigningKey = builder.Configuration["Jwt:DevKey"]!;
 }
 
-builder.Services.AddSingleton<IMongoClient>(sp =>
-        new MongoClient(mongoConnectionString));
-
-builder.Services.AddSingleton(sp =>
-    sp.GetRequiredService<IMongoClient>().GetDatabase(databaseName));
-
-builder.Services.Configure<MongoDbSettings>(
-    builder.Configuration.GetSection("MongoDbSettings")
-);
-
+// 3. Configuração do MongoDB e Repositórios
+builder.Services.AddSingleton<IMongoClient>(sp => new MongoClient(mongoConnectionString));
+builder.Services.AddSingleton(sp => sp.GetRequiredService<IMongoClient>().GetDatabase(databaseName));
 MongoMappings.ConfigureMappings();
-
-
-builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-builder.Services.AddAuthorization();
+// 4. Injeção de Dependência do AuthService com a chave JWT
+// Precisamos usar uma factory para injetar a string 'jwtSigningKey' que buscamos
+builder.Services.AddScoped<IAuthService>(sp =>
+    new AuthService(
+        sp.GetRequiredService<IUserRepository>(),
+        sp.GetRequiredService<IConfiguration>(),
+        sp.GetRequiredService<ILogger<AuthService>>(),
+        jwtSigningKey // Injetando a chave obtida do Parameter Store ou appsettings
+    ));
 
-builder.Services.AddControllers();
-builder.Services
-    .AddControllers()
+builder.Services.AddAuthorization();
+builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
     });
 
+// -- Resto da configuração (Swagger, Middlewares, etc.) --
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(opt =>
 {
-    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "FIAP Cloud Games - Games API", Version = "v1" });
-
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "FIAP Cloud Games - Auth API", Version = "v1" });
     opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
@@ -101,7 +105,6 @@ builder.Services.AddSwaggerGen(opt =>
         In = ParameterLocation.Header,
         Description = "Insira o token JWT no formato: Bearer {seu token}"
     });
-
     opt.AddSecurityRequirement(new OpenApiSecurityRequirement
    {
        {
@@ -118,19 +121,14 @@ builder.Services.AddSwaggerGen(opt =>
    });
 });
 
-
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseMiddleware<CorrelationIdMiddleware>();
 app.UseMiddleware<ExceptionHandler>();
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
